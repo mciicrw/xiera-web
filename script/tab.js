da951be44ed4563f5f547d1250c63ed800b26f34
mciicrw <yuda.pratama37@gmail.com>
2019-02-12 14:36:00 +0700

    added tab and table for commands

---
+++ script/tab.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+function openCity(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" template-green", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " template-green";
+}
