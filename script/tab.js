f047f8d1a311127bfd261841b722a30aa046c321
mciicrw <yuda.pratama37@gmail.com>
2019-02-12 15:15:45 +0700

    updated table and table script

--- script/tab.js
+++ script/tab.js
@@ -1,7 +1,7 @@
 // JavaScript Document
-function openCity(evt, cityName) {
+function openGroup(evt, groupName) {
   var i, x, tablinks;
-  x = document.getElementsByClassName("city");
+  x = document.getElementsByClassName("group");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
@@ -9,6 +9,6 @@ function openCity(evt, cityName) {
   for (i = 0; i < x.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" template-green", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(groupName).style.display = "block";
   evt.currentTarget.className += " template-green";
-}
+}	
